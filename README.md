85e6de6ddb22996b9ce94b97e9f39bb34d1cfbc7
ajouve <jouve.antoine.27@gmail.com>
2017-10-31 11:03:14 +0100

    Rename folder

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jodryanouk.github.io
 
+## Install
+
+   npm i
+
+## Build
+
+    npm run build
--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,19 @@ const sass = require('gulp-sass');
 const pug = require('gulp-pug');
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/main.scss')
+  return gulp.src('./docs/scss/main.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    .pipe(gulp.dest('./docs/css'));
 });
 
 gulp.task('views', function buildHTML() {
     return gulp.src(['views/about.pug', 'views/index.pug'])
     .pipe(pug({}))
-    .pipe(gulp.dest('./assets'));
+    .pipe(gulp.dest('./docs'));
 });
 
 gulp.task('build', ['sass', 'views']);
 
 gulp.task('sass:watch', function () {
-  gulp.watch('./assets/scss/*.scss', ['sass']);
+  gulp.watch('./docs/scss/*.scss', ['sass']);
 });
